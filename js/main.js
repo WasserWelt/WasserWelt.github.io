08f0817744ad7276fedbfcb9c2df22570cf6d325
WasserWelt <1051811795@qq.com>
2023-05-09 16:02:27 +0800

    Site updated: 2023-05-09 16:02:27

--- js/main.js
+++ js/main.js
@@ -1,3 +1,5 @@
+/* eslint-disable no-undef */
+// console.js
 console.log(String.raw`
           _____                    _____                    _____                    _____          
          /\    \                  /\    \                  /\    \                  /\    \         
@@ -21,4 +23,118 @@ console.log(String.raw`
         \:|   |                  \::/    /                \::/    /                \::/    /        
          \|___|                   \/____/                  \/____/                  \/____/         
 see theme at https://github.com/0x4qE/hexo-theme-Kaze
-`);const scrollWidth=document.body.scrollWidth||document.documentElement.scrollWidth;let darkControlButton=null;(darkControlButton=scrollWidth<=742?document.querySelector(".darkwidget"):document.querySelector(".darknavbar")).addEventListener("click",()=>{setDarkmode(reverseDarkModeSetting())});const smoothScrollToTop=()=>{let t=window.pageYOffset||document.body.scrollTop||document.documentElement.scrollTop;t>1?(window.requestAnimationFrame(smoothScrollToTop),scrollTo(0,Math.floor(.85*t))):scrollTo(0,0)};setTimeout(()=>{document.getElementById("scrollbutton").onclick=smoothScrollToTop},0);const reversePopButton=()=>{const t=document.getElementById("scrollbutton"),e=document.getElementById("menubutton"),o=document.getElementById("popbutton"),_=document.body.scrollWidth||document.documentElement.scrollWidth;"flex"===t.style.display?(t.style.bottom="32px",t.style.opacity="0",o.style.transform="none",setTimeout(()=>{t.style.display="none"},100)):(t.style.display="flex",o.style.transform="rotate(90deg)",setTimeout(()=>{t.style.bottom="85px",t.style.opacity="1"},100));const l=document.getElementById("mobiletoc");_<=862&&l&&("flex"===e.style.display?(e.style.right="32px",e.style.opacity="0",setTimeout(()=>{e.style.display="none"},100)):(e.style.display="flex",setTimeout(()=>{e.style.right="85px",e.style.opacity="1"},100)));const n=document.querySelector(".darkwidget");_<=742&&("flex"===n.style.display?(n.style.bottom="32px",n.style.opacity="0",n.style.transform="none",setTimeout(()=>{n.style.display="none"},100)):(n.style.display="flex",o.style.transform="rotate(90deg)",setTimeout(()=>{n.style.bottom="138px",n.style.opacity="1"},100)))};function menuClick(t){const e=t.target,o=document.getElementById("mobiletoc");o&&(o.contains(e)||(o.style.display="none",document.body.removeChild(mask),document.removeEventListener("click",menuClick)))}setTimeout(()=>{document.getElementById("popbutton").onclick=reversePopButton},0);const clickMenuButton=()=>{const t=document.getElementById("mobiletoc");if(!t)return;t.style.display="block";const e=document.createElement("div");e.id="mask",document.body.appendChild(e),setTimeout(()=>{document.addEventListener("click",menuClick)},0)};setTimeout(()=>{document.getElementById("menubutton").onclick=clickMenuButton},0);
+`);
+// darkmode.js
+// reverse button
+const scrollWidth = document.body.scrollWidth || document.documentElement.scrollWidth;
+let darkControlButton = null;
+if (scrollWidth <= 742) {
+  darkControlButton = document.querySelector('.darkwidget');
+} else {
+  darkControlButton = document.querySelector('.darknavbar');
+}
+
+darkControlButton.addEventListener('click', () => {
+  setDarkmode(reverseDarkModeSetting());
+});
+// scroll-up.js
+const smoothScrollToTop = () => {
+  let yTopValve = (window.pageYOffset || document.body.scrollTop || document.documentElement.scrollTop);
+  if (yTopValve > 1) {
+    window.requestAnimationFrame(smoothScrollToTop);
+    scrollTo(0, Math.floor(yTopValve * 0.85));
+  } else {
+    scrollTo(0, 0);
+  }
+};
+setTimeout(() => {
+  document.getElementById('scrollbutton').onclick = smoothScrollToTop;
+}, 0);
+// popbutton.js
+const reversePopButton = () => {
+  const scrollButton = document.getElementById('scrollbutton');
+  const menuButton = document.getElementById('menubutton');
+  const reverseButton = document.getElementById('popbutton');
+  const scrollWidth = document.body.scrollWidth || document.documentElement.scrollWidth;
+  if (scrollButton.style.display === 'flex') {
+    scrollButton.style.bottom = '32px';
+    scrollButton.style.opacity = '0';
+    reverseButton.style.transform = 'none';
+    setTimeout(() => {
+      scrollButton.style.display = 'none';
+    }, 100);
+  } else {
+    scrollButton.style.display = 'flex';
+    reverseButton.style.transform = 'rotate(90deg)';
+    setTimeout(() => {
+      scrollButton.style.bottom = '85px';
+      scrollButton.style.opacity = '1';
+    }, 100);
+  }
+  const mobileToc = document.getElementById('mobiletoc');
+  if (scrollWidth <= 862 && mobileToc) {
+    if (menuButton.style.display === 'flex') {
+      menuButton.style.right = '32px';
+      menuButton.style.opacity = '0';
+      setTimeout(() => {
+        menuButton.style.display = 'none';
+      }, 100);
+    } else {
+      menuButton.style.display = 'flex';
+      setTimeout(() => {
+        menuButton.style.right = '85px';
+        menuButton.style.opacity = '1';
+      }, 100);
+    }
+  }
+  const darkButton = document.querySelector('.darkwidget');
+  if (scrollWidth <= 742) {
+    if (darkButton.style.display === 'flex') {
+      darkButton.style.bottom = '32px';
+      darkButton.style.opacity = '0';
+      darkButton.style.transform = 'none';
+      setTimeout(() => {
+        darkButton.style.display = 'none';
+      }, 100);
+    } else {
+      darkButton.style.display = 'flex';
+      reverseButton.style.transform = 'rotate(90deg)';
+      setTimeout(() => {
+        darkButton.style.bottom = '138px';
+        darkButton.style.opacity = '1';
+      }, 100);
+    }
+  } 
+};
+setTimeout(() => {
+  document.getElementById('popbutton').onclick = reversePopButton;
+}, 0);
+// menuButton.js
+function menuClick(event) {
+  const target = event.target;
+  const mobileToc = document.getElementById('mobiletoc');
+  if (!mobileToc) {
+    return;
+  }
+  if (!mobileToc.contains(target)) {
+    mobileToc.style.display = 'none';
+    document.body.removeChild(mask);
+    document.removeEventListener('click', menuClick);
+  }
+}
+const clickMenuButton = () => {
+  const mobileToc = document.getElementById('mobiletoc');
+  if (!mobileToc) {
+    return;
+  }
+  mobileToc.style.display = 'block';
+  const mask = document.createElement('div');
+  mask.id = 'mask';
+  document.body.appendChild(mask);
+  setTimeout(() => {
+    document.addEventListener('click', menuClick);
+  }, 0);
+};
+setTimeout(() => {
+  document.getElementById('menubutton').onclick = clickMenuButton;
+}, 0);
